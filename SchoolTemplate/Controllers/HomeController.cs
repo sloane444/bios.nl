0c694dfbca0e2c1574005c9c177c98f81615d444
Remy v.d. Bosch <110032@st-maartenscollege.nl>
2020-09-14 15:34:15 +0200

    Film pagina aangemaakt

--- SchoolTemplate/Controllers/HomeController.cs
+++ SchoolTemplate/Controllers/HomeController.cs
@@ -8,56 +8,63 @@ using SchoolTemplate.Models;
 
 namespace SchoolTemplate.Controllers
 {
-  public class HomeController : Controller
-  {
-    // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
-    string connectionString = "Server=172.16.160.21;Port=3306;Database=110203;Uid=110203;Pwd=oNTragER;";
-
-    public IActionResult Index()
+    public class HomeController : Controller
     {
-      List<Product> products = new List<Product>();
-      // uncomment deze regel om producten uit je database toe te voegen
-      products = GetProducts();
+        // zorg ervoor dat je hier je gebruikersnaam (leerlingnummer) en wachtwoord invult
+        string connectionString = "Server=172.16.160.21;Port=3306;Database=110203;Uid=110203;Pwd=oNTragER;";
 
-      return View(products);
-    }
-
-    private List<Product> GetProducts()
-    {
-      List<Product> products = new List<Product>();
-
-      using (MySqlConnection conn = new MySqlConnection(connectionString))
-      {
-        conn.Open();
-        MySqlCommand cmd = new MySqlCommand("select * from product", conn);
-
-        using (var reader = cmd.ExecuteReader())
+        public IActionResult Index()
         {
-          while (reader.Read())
-          {
-            Product p = new Product
-            {
-              Id = Convert.ToInt32(reader["Id"]),
-              Naam = reader["Naam"].ToString(),
-              Calorieen = float.Parse(reader["calorieen"].ToString()),
-              Formaat = reader["Formaat"].ToString(),
-              Gewicht = Convert.ToInt32(reader["Gewicht"].ToString()),
-              Prijs = Decimal.Parse(reader["Prijs"].ToString())
-            };
-            products.Add(p);
-          }
+            List<Product> products = new List<Product>();
+            // uncomment deze regel om producten uit je database toe te voegen
+            products = GetProducts();
+
+            return View(products);
         }
-      }
 
-      return products;
-    }
+        private List<Product> GetProducts()
+        {
+            List<Product> products = new List<Product>();
 
-    public IActionResult Privacy()
-    {
-      return View();
-    }
+            using (MySqlConnection conn = new MySqlConnection(connectionString))
+            {
+                conn.Open();
+                MySqlCommand cmd = new MySqlCommand("select * from product", conn);
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        Product p = new Product
+                        {
+                            Id = Convert.ToInt32(reader["Id"]),
+                            Naam = reader["Naam"].ToString(),
+                            Calorieen = float.Parse(reader["calorieen"].ToString()),
+                            Formaat = reader["Formaat"].ToString(),
+                            Gewicht = Convert.ToInt32(reader["Gewicht"].ToString()),
+                            Prijs = Decimal.Parse(reader["Prijs"].ToString())
+                        };
+                        products.Add(p);
+                    }
+                }
+            }
+
+            return products;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+            [Route("films")]
+        public IActionResult Films()
+        {
+            return View();
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
       return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
